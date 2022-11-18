878e2b331768a0ae67b94ff0cdf80dcf6be19bb9
Fiziev Vagif <vagasuper560469@gmail.com>
2022-11-18 12:02:36 +0600

    Task 1.
    Added Movies components.

---
+++ src/Components/FormMovies/FormMovies.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FormMovies extends Component {
+  render() {
+    return (
+      <div>
+        Form
+      </div>
+    );
+  }
+}
+
+export default FormMovies;
--- src/Containers/Movies/Movies.tsx
+++ src/Containers/Movies/Movies.tsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import MoviesCard from "../../Components/MoviesCard/MoviesCard";
+import FormMovies from "../../Components/FormMovies/FormMovies";
 
 class Movies extends Component {
   render() {
     return (
       <div>
-        <p>Vagif</p>
+        <FormMovies/>
+        <MoviesCard/>
       </div>
     );
   }
